5c0aebed7aebc47b53a50a550cdf4f09bbb53094
USERNAME-WAS-TOOK <115166260+USERNAME-WAS-TOOK@users.noreply.github.com>
2022-12-06 08:18:15 -0500

    Player.gd

---
+++ Player.gd
@@ -0,0 +1,72 @@
+extends KinematicBody2D
+
+export(PackedScene) var BOTTLE: PackedScene = preload("res://Projectiles/PlayerBottle.tscn")
+
+onready var attackTimer = $AttackTimer
+
+const UP = Vector2(0,-1)
+const GRAVITY = 20
+const MAXFALLSPEED = 500
+const MAXSPEED = 175
+const JUMPFORCE = 400
+const ACCEL = 10
+
+var motion = Vector2()
+var facing_right = true
+
+func _ready():
+	pass 
+
+func throw_bottle(bottle_direction: Vector2):
+	if BOTTLE:
+		var bottle = BOTTLE.instance()
+		get_tree().current_scene.add_child(bottle)
+		bottle.global_position = self.global_position
+		
+		var bottle_rotation = bottle_direction.angle()
+		bottle.rotation = bottle_rotation
+		
+		attackTimer.start()
+
+func _physics_process(delta):
+	
+	motion.y += GRAVITY
+	if motion.y > MAXFALLSPEED:
+		motion.y = MAXFALLSPEED
+		
+	if facing_right == true:
+		$Sprite.scale.x = -1
+	else:
+		$Sprite.scale.x = 1
+	
+	motion.x = clamp(motion.x,-MAXSPEED,MAXSPEED)
+	
+	if Input.is_action_pressed("right"):
+		motion.x += ACCEL
+		facing_right = true
+		$AnimationPlayer.play("Run")
+	elif Input.is_action_pressed("left"):
+		motion.x -= ACCEL
+		facing_right = false
+		$AnimationPlayer.play("Run")
+	else:
+		motion.x = lerp(motion.x,0,0.2)
+		$AnimationPlayer.play("Idle")
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			motion.y = -JUMPFORCE
+		if !is_on_floor():
+			if motion.y < 0:
+				$AnimationPlayer.play("Jump")
+			elif motion.y > 0:
+				$AnimationPlayer.play("Fall")
+	
+	motion = move_and_slide(motion,UP)
+	
+	#throw_bottle does not currently work - line 71, cannot convert argument 1 from bool to Vector2
+	
+	if Input.is_action_just_pressed("action_attack") and attackTimer.is_stopped():
+		var bottle_direction = facing_right
+		throw_bottle(bottle_direction)
+		$AnimationPlayer.play("Throw")
