7a628ff51438b210e7f8da94717fd800e9bd1ad1
USERNAME-WAS-TOOK <115166260+USERNAME-WAS-TOOK@users.noreply.github.com>
2022-12-07 08:32:29 -0500

    Create PlayerBottle.gd

---
+++ PlayerBottle.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+export(int) var SPEED: int = 100
+
+func _physics_process(delta):
+	var direction = Vector2.RIGHT.rotated(rotation)
+	global_position += SPEED * direction * delta
+
+func destroy():
+	queue_free()
+
+func _on_PlayerBottle_area_entered(area):
+	destroy()
+
+func _on_PlayerBottle_body_entered(body):
+	destroy()
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
