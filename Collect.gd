d25b3112db4dc5e975355d8a90bbdc7c840c68b8
USERNAME-WAS-TOOK <115166260+USERNAME-WAS-TOOK@users.noreply.github.com>
2023-01-12 10:02:53 -0500

    Create Collect.gd

---
+++ Collect.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+# warning-ignore:unused_argument
+func _on_Bottle_Pack_body_entered(body):
+	queue_free()
+	pass # Replace with function body.
+
+
+func _on_Bottle_Single_body_entered(body):
+	pass # Replace with function body.
